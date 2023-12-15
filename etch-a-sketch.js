24d410f7bb60be911b130b3ee18be3d05d190064
corwin-bell <corwin.m.bell@gmail.com>
2023-12-15 10:06:27 -0700

    create grid squares with loop

---
+++ etch-a-sketch.js
@@ -0,0 +1,10 @@
+// create a 16 x 16 grid of divs inside the gridContainer using JS
+// define the container to store the grid
+let gridContainer = document.querySelector('.gridContainer');
+
+// loop creation of squares
+for (let i = 1; i < (16*16); i++) {
+    let gridSquare = document.createElement('div');
+    gridSquare.className = 'gridSquare';
+    gridContainer.appendChild(gridSquare);
+}

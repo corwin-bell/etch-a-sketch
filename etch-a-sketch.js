2337b47119540001e1e9dc8f8d9ce7e26e10381c
corwin-bell <corwin.m.bell@gmail.com>
2023-12-15 12:25:55 -0700

    basic grid creation without grid cell resize

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,20 +1,43 @@
 // create a 16 x 16 grid of divs inside the gridContainer using JS
 // define the container to store the grid
-let gridContainer = document.querySelector('.gridContainer');
+const createGridButton = document.querySelector('button');
+const gridContainer = document.querySelector('.gridContainer');
 
-// loop creation of squares
+// create initial grid squares
 for (let i = 1; i <= (16*16); i++) {
     let gridSquare = document.createElement('div');
     gridSquare.className = 'gridSquare';
     gridContainer.appendChild(gridSquare);
 }
 
-// add hover effect, when mouse passing through
-const grid = document.querySelectorAll('.gridSquare');
+// create gridSquare nodelist after creation
+let grid = document.querySelectorAll('.gridSquare');
+
+// add sketching effect to grid
 grid.forEach((gridSquare) => {
     gridSquare.addEventListener('mouseenter', () => {
         gridSquare.style.backgroundColor = 'gray';
     })
-})
-// grid square chages color
-// leaves a trail like a pen stroke
+});
+
+// create new grid with desired effects on click
+// on button click, prompt user for gridSquareNum
+    createGridButton.addEventListener('click', () => {
+        // accept 100 or less grid squares as input
+        let gridSquareNum = prompt('Enter number of desired grid squares (must be less than 100).',0);
+        // removes existing grid
+        grid.forEach(gridSquare => gridSquare.remove());
+        // adds new grid that fills same space
+        for (let i = 1; i <= (gridSquareNum*gridSquareNum); i++) {
+            gridSquare = document.createElement('div');
+            gridSquare.className = 'gridSquare';
+            gridContainer.appendChild(gridSquare);
+        }
+        grid = document.querySelectorAll('.gridSquare');
+        grid.forEach((gridSquare) => {
+            gridSquare.addEventListener('mouseenter', () => {
+                gridSquare.style.backgroundColor = 'gray';
+            })
+        });
+    });
+
